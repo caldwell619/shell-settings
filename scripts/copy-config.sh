9733e1baba7e589fce96c782859e4f4083c8f0b5
caldwell619 <caldwell8814@gmail.com>
2020-03-26 21:07:06 -0500

    initial

---
+++ scripts/copy-config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BICyan="\033[1;96m"    # Bold Cyan
+
+# Reset
+Color_Off="\033[0m"    # Text Reset
+
+printf "\n\n$BICyan$( echo Setting up FISH config )$Color_Off"
+cp ./config/config.fish ~/.config/fish/config.fish
+source ~/.config/fish/config.fish
---
+++ scripts/fish.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+Red="\033[0;31m"       # Red
+Green="\033[0;32m"     # Green
+BICyan="\033[1;96m"    # Bold Cyan
+
+# Reset
+Color_Off="\033[0m"    # Text Reset
+
+
+printf "\n\n$BICyan$( echo Installing FISH )$Color_Off"
+
+brew install fish
+
+curl -L https://get.oh-my.fish | fish
+
+# Installs fonts for theme
+# You MUST change the font on iTerm for this to work
+printf "\n\n$BICyan$( echo Installing fonts )$Color_Off"
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+
+
+# Removing fonts
+printf "\n\n$BICyan$( echo Cleaning up fonts )$Color_Off"
+cd ../
+rm -rf fonts
+
+set -g theme_nerd_fonts yes
+
+# Installs theme
+printf "\n\n$BICyan$( echo Installing 'bobthefish' theme )$Color_Off"
+omf install bobthefish
+omf theme bobthefish
+
+# Installing nvm wrapper
+printf "\n\n$BICyan$( echo Installing nvm wrapper )$Color_Off"
+omf install nvm
+
+
+# Sourcing the new env for fish
+source ./scripts/copy-config.sh
